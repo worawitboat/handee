e99dea6a0b39bc87636ca7e1c29fc1c3db932182
worawitboat <worawit.pa.61@ubu.ac.th>
2021-10-23 14:49:37 +0700

    update pam page

--- src/components/_pages/pam/index.js
+++ src/components/_pages/pam/index.js
@@ -12,6 +12,21 @@ export default function PamPage() {
   const { Search } = Input;
   const router = useRouter()
 
+  const countriesData = [
+    {
+      image: 'https://travel.mthai.com/app/uploads/2019/02/16730144_1867791716833973_2744032784152525809_n.jpg',
+      name: 'Korea',
+    },
+    {
+      image: 'http://www.thaifly.com/image/data/img_thaifly/article/switzerland/stmoritz.jpg',
+      name: 'Switzerland',
+    },
+    {
+      image: 'https://pix10.agoda.net/geo/city/2994/1_2994_02.jpg?s=1920x822',
+      name: 'Dubai',
+    },
+  ]
+
   const suffix = (
     <AudioOutlined
       style={{
@@ -24,110 +39,82 @@ export default function PamPage() {
   const { Meta } = Card;
 
   return (
-      <div>
-        <Flex justifyContent="center" style={{ marginLeft: 95 }}>
-          <Box width={[1 / 6]} p={4}>
-            Home
-          </Box>
-          <Box width={[1 / 6]} p={4}>
-            Tickets
-          </Box>
-          <Box width={[1 / 6]} p={4}>
-            Hotels
-          </Box>
-          <Box width={[1 / 6]} p={4}>
-            Car Hire
-          </Box>
-          <Box width={[1 / 6]} p={4}>
-            Contact
-          </Box>
-          <Box width={[1 / 6]} p={4}>
-            <Button type="primary" onClick={() => router.push("/signin")}>
-              SIGN IN
-            </Button>
-          </Box>
-        </Flex>
+    <div>
+      <Flex justifyContent="center" style={{ marginLeft: 95 }}>
+        <Box width={[1 / 6]} p={4}>
+          <p style={{ cursor: 'pointer' }}>Home</p>
+        </Box>
+        <Box width={[1 / 6]} p={4}>
+          Tickets
+        </Box>
+        <Box width={[1 / 6]} p={4}>
+          Hotels
+        </Box>
+        <Box width={[1 / 6]} p={4}>
+          Car Hire
+        </Box>
+        <Box width={[1 / 6]} p={4}>
+          Contact
+        </Box>
+        <Box width={[1 / 6]} p={4}>
+          <Button type="primary" onClick={() => router.push("/signin")}>
+            SIGN IN
+          </Button>
+        </Box>
+      </Flex>
+      <Flex flexDirection='column'>
         <Image
-        style={{ marginTop: 0.5 , marginLeft: 25 , marginRight: 30}}
-        width={1470}
-        height={350}
-        src="https://images.unsplash.com/photo-1500622944204-b135684e99fd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2061&q=80"/>
+          style={{ marginTop: 0.5, height: 700, width: '100%', padding: 10 }}
+          src="https://images.unsplash.com/photo-1500622944204-b135684e99fd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2061&q=80" />
         <Space direction="vertical">
-          <Search
-            style={{ marginTop: -200 , marginLeft: 380}}
-            placeholder="input search text"
-            allowClear
-            enterButton="Search"
-            size="large"
-            onSearch={onSearch}
-            width={200}
-          />
-        </Space>,
-        <Card
-          style={{ width: 400 , height:300 , marginTop: 10 , marginLeft: 90}}
-          cover={
-            <img
-              alt="example"
-              src="https://travel.mthai.com/app/uploads/2019/02/16730144_1867791716833973_2744032784152525809_n.jpg"
-            />
+          <Flex>
+            <Box width={1 / 3}></Box>
+            <Box width={1 / 3}>
+              <Search
+                style={{ marginTop: -200 }}
+                placeholder="input search text"
+                allowClear
+                enterButton="Search"
+                size="large"
+                onSearch={onSearch}
+              />
+            </Box>
+            <Box width={1 / 3}></Box>
+          </Flex>
+        </Space>
+        <Flex justifyContent="center">
+          {countriesData.map(item => {
+            return (
+              <Card
+                style={{ marginTop: 10, marginLeft: 90 }}
+                cover={
+                  <img
+                    style={{ width: 400, height: 200, }}
+                    alt="example"
+                    src={item.image}
+                  />
+                }
+                actions={[
+                  <SettingOutlined key="setting" />,
+                  <EditOutlined key="edit" />,
+                  <EllipsisOutlined key="ellipsis" />,
+                ]}
+              >
+                <Meta
+                  avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                  title={item.name}
+                  description="This is the description"
+                />
+              </Card>
+            )
+          })
+
           }
-          actions={[
-            <SettingOutlined key="setting" />,
-            <EditOutlined key="edit" />,
-            <EllipsisOutlined key="ellipsis" />,
-          ]}
-        >
-          <Meta
-            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-            title="Korea"
-            description="This is the description"
-          />
-        </Card>,
-        <Card
-          style={{ width: 400 , height:300 , marginTop: -323 , marginLeft: 560}}
-          cover={
-            <img
-              alt="example"
-              src="http://www.thaifly.com/image/data/img_thaifly/article/switzerland/stmoritz.jpg"
-            />
-          }
-          actions={[
-            <SettingOutlined key="setting" />,
-            <EditOutlined key="edit" />,
-            <EllipsisOutlined key="ellipsis" />,
-          ]}
-        >
-          <Meta
-            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-            title="Switzerland"
-            description="This is the description"
-          />
-        </Card>,
-        <Card
-          style={{ width: 400 , height:300 , marginTop: -323 , marginLeft: 1030}}
-          cover={
-            <img
-              alt="example"
-              src="https://pix10.agoda.net/geo/city/2994/1_2994_02.jpg?s=1920x822"
-            />
-          }
-          actions={[
-            <SettingOutlined key="setting" />,
-            <EditOutlined key="edit" />,
-            <EllipsisOutlined key="ellipsis" />,
-          ]}
-        >
-          <Meta
-            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-            title="Dubai"
-            description="This is the description"
-          />
-        </Card>,
-        
-        
-      </div>
-      
-    
-      
+        </Flex>
+      </Flex>
+    </div>
+
+
+
   );
 }
