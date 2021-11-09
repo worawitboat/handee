77d8c108be76cc95c727b58d1a3b9b803a0e4cc1
worawitboat <worawit.pa.61@ubu.ac.th>
2021-11-09 15:42:22 +0700

    grid and submit

--- src/components/_pages/SelectHandOrder/index.js
+++ src/components/_pages/SelectHandOrder/index.js
@@ -1,131 +1,49 @@
-import React from 'react'
-import { Flex } from 'reflexbox'
-import {Button, Input} from 'antd';
+import React, { useState } from 'react'
+import { Flex, Box } from 'reflexbox'
+import { Button, Input } from 'antd';
 import router from 'next/router'
 import styled from '@emotion/styled';
 
 
 export default function SelectHandOrder(props) {
-    const CommitForm = () => {
-        // let Names = document.getElementById('names').value;
-        // let Lastnames = document.getElementById('lastnames').value;
-        // let Locations = document.getElementById('locations').value;
-        // let Phonenumbers = document.getElementById('phonenumbers').value;
-        // const x = [];
-        // x.push(Names , Lastnames , Locations , Phonenumbers);
-        router.push('/DetailProHand')
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [address, setAddress] = useState("")
+    const [phone, setPhone] = useState("")
+
+    function submit() {
+        const data = {
+            firstName: firstName,
+            lastName: lastName,
+            address: address,
+            phone: phone
+        }
+        console.log(data);
     }
 
-const InputName = styled(Input)`
-    width:100%;    
-    font-size:12px;
-    border-radius: 50px;
-    padding:5px 5px 5px 40px;
-`
 
-const InputLastname = styled(Input)`
-    width:100%;    
-    font-size:12px;
-    border-radius: 50px;
-    padding:5px 5px 5px 70px;
-`
-
-const InputLocation = styled(Input)`
-    width:100%;    
-    font-size:12px;
-    border-radius: 50px;
-    padding:5px 5px 5px 48px;
-`
-
-const InputPhonenumber = styled(Input)`
-    width:100%;    
-    font-size:12px;
-    border-radius: 50px;
-    padding:5px 5px 5px 68px;
-`
-
-
-const Div = styled.div `
-    position:relative;
-    `
-
-const LabelName = styled.label
-`
-    position:absolute;
-	left:15px;
-	font-weight:bold;
-	font-size:0.8em;
-	color:#444;
-	top:8px;
-`
-
-const LabelLastName = styled.label
-`
-    position:absolute;
-	left:15px;
-	font-weight:bold;
-	font-size:0.8em;
-	color:#444;
-	top:58px;
-`
-
-const LabelLocation = styled.label
-`
-    position:absolute;
-	left:15px;
-	font-weight:bold;
-	font-size:0.8em;
-	color:#444;
-	top:112px;
-`
-
-const LabelPhone = styled.label
-`
-    position:absolute;
-	left:15px;
-	font-weight:bold;
-	font-size:0.785em;
-	color:#444;
-	top:162px;
-`
-
-const Buttons = styled(Button)`
-width:130px;
-height:40px;
-font-weight:bold;
-font-size:15px;
-border-radius: 50px;
-`
-
-
-return (
+    return (
         <>
-            <Flex justifyContent="center" style={{marginTop: 100}}> 
-                <Flex  flexDirection="column" textAlign="center">
-                <h2><b>กรอกข้อมูลสำหรับคนฝากหิ้ว</b></h2><br/>              
-                   <Div>
-                   <InputName type="text" id="names" />
-                   <LabelName>ชื่อ :</LabelName><br/><br/>
+            <Flex justifyContent="center" style={{ paddingTop: 100 }}>
+                <Flex flexDirection="column" textAlign="center">
+                    <h2>กรอกข้อมูลสำหรับคนฝากหิ้ว</h2>
+                    <Input placeholder="ชื่อ" style={{ marginTop: 20, borderRadius: 50}} onChange={(e)=> setFirstName(e.target.value)}/>
+                    <Input placeholder="นามสกุล" style={{ marginTop: 20, borderRadius: 50 }} onChange={(e)=> setLastName(e.target.value)}/>
+                    <Input placeholder="ที่อยู่" style={{ marginTop: 20, borderRadius: 50 }} onChange={(e)=> setAddress(e.target.value)}/>
+                    <Input placeholder="เบอร์โทรศัพท์" style={{ marginTop: 20, borderRadius: 50 }} onChange={(e)=> setPhone(e.target.value)}/>
+                    <Flex justifyContent="center" style={{ marginTop: 20, borderRadius: 50 }} >
+                        <Box width={1/3}>
+                            <Button type="primary" style={{ borderRadius: 20 }} onClick={()=> submit()}>ยืนยัน</Button>
+                        </Box>
+                        <Box width={1/3}>
 
-                   <InputLastname type="text" id="lastnames"/>
-                   <LabelLastName>นามสกุล :</LabelLastName><br/><br/>
-
-                   <InputLocation type="text" id="locations"/>
-                   <LabelLocation>ที่อยู่ :</LabelLocation><br/><br/>
-
-                   <InputPhonenumber type="text" id="phonenumbers"/>
-                   <LabelPhone>เบอร์โทร :</LabelPhone><br/><br/>
-                    </Div>
-                   <Flex justifyContent="center"style={{marginTop: 25}}>
-                       <div style={{paddingRight:25}}>
-                       <Buttons onClick={CommitForm} type="primary">ยืนยัน</Buttons>
-                       </div>
-                        <div>
-                        <Buttons onClick={()=>{router.push('/')}} type="danger">ยกเลิก</Buttons>
-                        </div>
-                        
-                   </Flex>
+                        </Box>
+                        <Box width={1/3}>
+                            <Button type="primary" danger style={{ borderRadius: 20 }} onClick={()=> router.push('/')}>ยกเลิก</Button>
+                        </Box>
+                    </Flex>
                 </Flex>
+
             </Flex>
         </>
     )
--- src/components/_pages/SelectPerson/index.js
+++ src/components/_pages/SelectPerson/index.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import {Flex} from 'reflexbox';
-import {Grid, Col, Row} from 'antd'
-import {BtnSelectPerson} from '../../_common';
+import { Flex, Box } from 'reflexbox';
+import { BtnSelectPerson } from '../../_common';
 
 export default function SelectPerson(props) {
     return (
-        <> {/* <Flex justifyContent="center" style={{marginTop: 250}}>
-                <BtnSelectPerson btnName="คนฝากหิ้ว" RouterWay="/SelectHandOrder"/>
-                <BtnSelectPerson btnName="คนรับหิ้ว"  RouterWay="/SelectOrderHand"/>                    
-            </Flex> */}
-
-            <Flex justifyContent="center"style={{marginTop: 250}}>
-                <grid>
-                    <Row>
-                        <Col><BtnSelectPerson btnName="คนฝากหิ้ว" RouterWay="/SelectHandOrder"/></Col>
-                        <Col><BtnSelectPerson btnName="คนฝากหิ้ว" RouterWay="/SelectHandOrder"/></Col>
-                    </Row>
-                </grid>
+        <>
+            <Flex justifyContent="center" style={{ marginTop: 250 }}>
+                <BtnSelectPerson btnName="คนฝากหิ้ว" RouterWay="/SelectHandOrder" />
+                <BtnSelectPerson btnName="คนรับหิ้ว" RouterWay="/SelectOrderHand" />
             </Flex>
-
         </>
     )
 }
