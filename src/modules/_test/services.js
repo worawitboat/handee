4ce31b0339edeb9821ccf22d7c878e2661528483
worawitboat <worawit.pa.61@ubu.ac.th>
2021-11-16 23:33:28 +0700

    uipdate

--- src/modules/_test/services.js
+++ src/modules/_test/services.js
@@ -102,7 +102,7 @@ export function paymentimage(data) {
 
 export function getUserOrderById(id) {
 
-    return axios.get(`${apiUrl}/userorder/getbyId/${id}`)
+    return axios.post(`${apiUrl}/userorder/${id}`)
         .catch(({ response }) => {
             throw new Error(response.data.message)
         })
